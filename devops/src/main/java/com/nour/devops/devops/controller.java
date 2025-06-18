e9f028d0ca7071fd685ac3f31796ed7506e5e18c
nourashraf2 <102329754+nourashraf2@users.noreply.github.com>
2025-06-19 02:04:58 +0300

    Initial commit

---
+++ devops/src/main/java/com/nour/devops/devops/controller.java
@@ -0,0 +1,16 @@
+package com.nour.devops.devops;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!";
+    }
+}
+
+
